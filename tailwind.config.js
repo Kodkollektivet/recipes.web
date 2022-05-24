ae60f9ce9d5d03d090d91239a1f03f0d2ba5525d
Shoeb Chowdhury <sc222rf@student.lnu.com>
2022-05-24 15:56:41 +0200

    base

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        
+        brightRed: 'hsl(12,88%,59%)',
+        veryLightGray: 'hsl(0,0%,98%)',
+      },
+    }
+    
+  },
+  plugins: [],
+}
